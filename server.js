e2d17873994679bf9ee3d2ff2db8d1e38e8a4a0b
Brandon Rodriguez <brandonart1224@gmail.com>
2019-10-01 17:52:32 -0700

    push build folder

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const cookieSession = require('cookie-session');
 const methodOverride = require('method-override')
 const flash = require('connect-flash');
@@ -81,17 +80,17 @@ require('./routes/serviceRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
   //Express will serve up production assets
   //Like our main.js file, or main.css file
-  app.use(express.static('/client/build'));
+  app.use(express.static('client/build'));
 
   //Express will serve up the index.html file
   //if it doesn't recognize the route
 
   const path = require('path');
   app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+      const index = path.join(__dirname, 'build', 'index.html');
+      res.sendFile(index);
   });
 }
-
 
 const PORT = process.env.PORT || 5000;
 
