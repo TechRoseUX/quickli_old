102570c95ebfa1f0ca2a189a9e706b7f28fe7ce0
Brandon Rodriguez <brandonart1224@gmail.com>
2019-11-15 16:35:51 -0800

    client build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a14d8720ccb98e6788f0cd6b53bd30b4.js"
+  "/precache-manifest.15d5b8ac8947a2246a089bf9510b4838.js"
 );
 
 self.addEventListener('message', (event) => {
